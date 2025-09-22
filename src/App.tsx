39f270b6dd13b2400f584bc4c3f086fa1e3a6894
prabhjotsingh <prabhjotsingh6@academic.rrc.ca>
2025-09-21 21:36:10 -0500

    Added styling to the component of the application

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,9 @@ import ParticipantList from "./components/participant-manager/ParticipantList";
 function App() {
   return (
     <div className="participant-list">
-      <header>
-        <h1>EventHub</h1>
-      </header>
-
       <main>
         <ParticipantList />
       </main>
-
-      <footer>
-        <p>Team Members: Daljeet Kaur,Sukhtab Singh Warya, Prabhjot Singh, Arshnoorpreet Kaur</p>
-      </footer>
     </div>
   );
 }
