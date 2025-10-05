0614620951e8333f8c198e5e8b3df7ede3960492
swarya <swarya@academic.rrc.ca>
2025-10-05 12:45:57 -0500

    Sprint 2: Added add/remove functionality and form in Past Events component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "../src/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
- import EventCreator from "./components/event-creator/EventCreator";
+import { useState } from "react";   
+
+import EventCreator from "./components/event-creator/EventCreator";
 import ScoreTracking from "./components/score-tracker/score";
 import PastEvents from "./components/event-history/PastEvents";
 import ParticipantList from "./components/participant-manager/ParticipantList";
@@ -8,6 +10,8 @@ import Header from "./Header/header";
 import Footer from "./Footer/footer";
 
 function App() {
+  const [message, setMessage] = useState("Welcome to EventHub Shared State!");
+
   return (
     <Router>
       <div className="app">
@@ -18,7 +22,7 @@ function App() {
             <Route path="/create" element={<EventCreator />} />
             <Route path="/scores" element={<ScoreTracking />} />
             <Route path="/participants" element={<ParticipantList />} />
-            <Route path="/past" element={<PastEvents />} />
+            <Route path="/past" element={<PastEvents message={message} setMessage={setMessage} />} />
           </Routes>
         </main>
         <Footer />
@@ -26,5 +30,5 @@ function App() {
     </Router>
   );
 }
- 
+
 export default App;
