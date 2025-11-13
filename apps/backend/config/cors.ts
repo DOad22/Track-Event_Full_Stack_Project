03db06486d3d26b59f7b4d206512be80d5352401
Daljeet <daljeetkaur@rrc.ca>
2025-11-13 13:52:26 -0600

    Sprint 4 done the part for event creator

---
+++ apps/backend/config/cors.ts
@@ -0,0 +1,11 @@
+import type { CorsOptions } from "cors";
+ 
+const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173";
+ 
+const corsOptions: CorsOptions = {
+  origin: allowedOrigin,
+  optionsSuccessStatus: 200,
+};
+ 
+export default corsOptions;
+ 
